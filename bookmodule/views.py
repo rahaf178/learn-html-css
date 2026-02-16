4bee9c3f17a2606d11a11061c4a426cafbafe19b
Rahaf <r22581919@gmail.com>
2026-02-16 22:20:23 +0300

    TASK5

--- bookmodule/views.py
+++ bookmodule/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render 
 
-def index(request): 
+def index(request):
     name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html")    
+    return render(request, "bookmodule/index.html" , {"name": name})  
+    
 
 
 
