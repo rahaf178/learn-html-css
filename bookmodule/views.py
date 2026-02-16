20eb0d543fb6b1e0df06ad008a95533aa1b17a1e
Rahaf <r22581919@gmail.com>
2026-02-16 22:06:09 +0300

    Task4

--- bookmodule/views.py
+++ bookmodule/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render 
 
-def index(request):
+def index(request): 
     name = request.GET.get("name") or "world!"
-    return HttpResponse("Hello, "+name)
- 
-def index2(request, val1 = 0):   #add the view function (index2)
-    return HttpResponse("value1 = "+str(val1))
+    return render(request, "bookmodule/index.html")    
+
 
 
 
