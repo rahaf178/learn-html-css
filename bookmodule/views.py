fc9400c74319b433d6734b4a8e510097db903deb
Rahaf <r22581919@gmail.com>
2026-02-16 21:41:51 +0300

    update index function

--- bookmodule/views.py
+++ bookmodule/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    name = request.GET.get("name") or "world!"
-    return HttpResponse("Hello, "+name)
- 
-def index2(request, val1 = 0):   #add the view function (index2)
-    return HttpResponse("value1 = "+str(val1))
+    return HttpResponse("Hello, world!")
+
 
 
