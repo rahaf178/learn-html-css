b9fd516aa17f9b5a30d544524c45f20766763511
Rahaf <r22581919@gmail.com>
2026-02-16 23:31:16 +0300

    Task7

--- bookmodule/urls.py
+++ bookmodule/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('index2/<val1>/', views.index2)
+    path('index2/<val1>/', views.index2),
+    path('<int:bookId>', views.viewbook),
 ]
--- bookmodule/views.py
+++ bookmodule/views.py
@@ -14,6 +14,16 @@ def index2(request, val1):
         return HttpResponse("error, expected val1 to be integer")
 
     
+def viewbook(request, bookId):
+    # assume that we have the following books somewhere (e.g. database)
+    book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
+    book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    targetBook = None
+    if book1['id'] == bookId: targetBook = book1
+    if book2['id'] == bookId: targetBook = book2
+    context = {'book':targetBook} # book is the variable name accessible by the template
+    return render(request, 'bookmodule/show.html', context)
+
 
 
 
