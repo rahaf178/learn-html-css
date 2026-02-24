01caba5a01847f0daaf562e4b540c67cc4a30ff0
Rahaf <r22581919@gmail.com>
2026-02-24 03:09:51 +0300

    update

--- bookmodule/views.py
+++ bookmodule/views.py
@@ -24,6 +24,19 @@ def viewbook(request, bookId):
     context = {'book':targetBook} # book is the variable name accessible by the template
     return render(request, 'bookmodule/show.html', context)
 
+def index(request):
+    return render(request, "bookmodule/index.html")
+ 
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+ 
+def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+ 
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
+
+
 
 
 
